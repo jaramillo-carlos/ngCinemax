6fa044121aad255a7ae34db0f1a71ea9a98e6815
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-17 14:19:15 -0500

    creating carousel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MoviesFilterComponent } from './landing-page/movies-filter/movies-filte
 import { SlidesComponent } from './landing-page/slides/slides.component';
 import { WeeklyBillboardsComponent } from './landing-page/weekly-billboards/weekly-billboards.component';
 import { PremieresComponent } from './landing-page/premieres/premieres.component';
+import { CarouselComponent } from './landing-page/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PremieresComponent } from './landing-page/premieres/premieres.component
     MoviesFilterComponent,
     SlidesComponent,
     WeeklyBillboardsComponent,
-    PremieresComponent
+    PremieresComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
