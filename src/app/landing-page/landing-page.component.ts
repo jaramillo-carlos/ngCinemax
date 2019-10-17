db946a7404fef66c8e72a49a5ef957f1ad9c069a
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-17 11:53:29 -0500

    add assets and movies slider

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-landing-page',
@@ -10,6 +11,7 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.slider').slider();
   }
 
 }
