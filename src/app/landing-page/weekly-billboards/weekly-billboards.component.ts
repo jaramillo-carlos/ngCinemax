a32e9a30937b86461629800191fb376029a6fa71
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-17 22:22:31 -0500

    eekly billboards tests

--- src/app/landing-page/weekly-billboards/weekly-billboards.component.ts
+++ src/app/landing-page/weekly-billboards/weekly-billboards.component.ts
@@ -1,3 +1,5 @@
+import { Movie } from './../../models/movie.model';
+import { LandingPageService } from './../../services/landing-page.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weekly-billboards.component.css']
 })
 export class WeeklyBillboardsComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[];
+  constructor(public _landingService: LandingPageService) { }
 
   ngOnInit() {
+    this.getWeeklyBillBoards();
+  }
+
+  getWeeklyBillBoards() {
+    this._landingService.getMovies().subscribe( (movies: Movie[]) => {
+      this.movies = movies;
+    });
   }
 
 }
